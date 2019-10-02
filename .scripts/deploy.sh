9a0c94842ecfac23210dbd559afd16bd13945aca
shekharrajak <shekharstudy@ymail.com>
2019-10-02 11:55:53 +0530

    deploy the hugo project script added

---
+++ .scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install -g surge
+
+DEPLOY_DOMAIN = pr-$PR-numpy.org-newsite.surge.sh
+DEPLOY_PATH = "./public"
+echo ${github.actor}
+echo $DEPLOY_DOMAIN
+echo $PR
+
+surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
