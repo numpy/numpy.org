0025546d2701a1f67a2ca9ef72fcb2cb6127eeab
shekharrajak <shekharstudy@ymail.com>
2019-10-19 13:14:45 +0530

    minor change

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,9 +8,7 @@ PR_NUMBER="$(echo $REF | cut -d'/' -f3)"
 PROJECT_BUILD="./public"
 DOMAIN="pr-${PR_NUMBER}-numpy.org-newsite.surge.sh"
 echo "PR_NUMBER: $PR_NUMBER"
-pwd
 ls
-ls "./public"
 
 surge --project $PROJECT_BUILD --domain $DOMAIN;
 
