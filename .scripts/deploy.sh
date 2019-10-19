b239f535ee90171d8f1cd634e7e389f31385c95c
shekharrajak <shekharstudy@ymail.com>
2019-10-19 12:55:38 +0530

    variables used in deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,9 +5,11 @@ npm install -g surge
 echo "REF value is ${REF}"
 
 PR_NUMBER="$(echo $REF | cut -d'/' -f3)"
+PROJECT_BUILD="./public"
+DOMAIN="pr-${PR_NUMBER}-numpy.org-newsite.surge.sh"
 echo "PR_NUMBER: $PR_NUMBER"
 
 
-surge --project "./public" --domain "pr-${PR_NUMBER}-numpy.org-newsite.surge.sh";
+surge --project PROJECT_BUILD --domain DOMAIN;
 
-echo ::set-output name=deployed-domain::"pr-${PR_NUMBER}-numpy.org-newsite.surge.sh"
+echo ::set-output name=deployed-domain::DOMAIN
