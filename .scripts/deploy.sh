ba22dbd007f444db4feb38a773256c5d62150085
shekharrajak <shekharstudy@ymail.com>
2019-10-19 13:05:20 +0530

    minor change - checking the current directory it is looking for project build

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -9,6 +9,7 @@ PROJECT_BUILD="./public"
 DOMAIN="pr-${PR_NUMBER}-numpy.org-newsite.surge.sh"
 echo "PR_NUMBER: $PR_NUMBER"
 pwd
+ls
 ls "./public"
 
 surge --project $PROJECT_BUILD --domain $DOMAIN;
