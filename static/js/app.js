3b4911dfe6a10712f5be1d8969a7ab5dbbe3164d
Joe LaChance <jlachance1@gmail.com>
2019-11-14 22:17:01 -0600

    hero for mobile

--- static/js/app.js
+++ static/js/app.js
@@ -4,10 +4,13 @@ $(function () {
       const exists = $('.thebelab-cell').length;
       if (exists) {
         $('.thebelab-cell').attr('id', 'demo-code');
+        $('#demo-code').css('position', 'relative');
+        $('#demo-code').prepend('<div class="demo-caret">&gt;</div>');
         $('.thebelab-input').attr('id', 'demo-input');
         $('.thebelab-button').each(function(idx) {
           if (idx == 0) {
             $(this).attr('id', 'demo-button-run');
+            $(this).html('Run <span class="shift-enter">(Shift + Enter)</span>');
           } else {
             $(this).remove();
           }
