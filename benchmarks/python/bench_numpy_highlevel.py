721f025aa2a3502d06d362f90951a85236147f52
Khushi Agrawal <khushiagrawal411@gmail.com>
2021-08-10 09:19:58 +0530

    Edited

--- benchmarks/python/bench_numpy_highlevel.py
+++ benchmarks/python/bench_numpy_highlevel.py
@@ -117,7 +117,8 @@ if __name__ == "__main__":
     masses, positions, velocities = load_input_data(path_input)
     
     start = time.time()
-    energy, energy0 = numpy_highlevel(time_step, number_of_steps, masses, positions, velocities)
+    for i in range(5):
+        energy, energy0 = numpy_highlevel(time_step, number_of_steps, masses, positions, velocities)
     end = time.time()
 
     print(f"Final dE/E = {(energy - energy0) / energy0:.7e}")
