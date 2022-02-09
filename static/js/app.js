2cd86c0e9169ebf4efeed9e5080bca789db73829
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-10 00:23:45 +0100

    Use JupyterLite to try `numpy` in the browser (#547)
    * Use JupyterLite to try `numpy` in the browser
    
    * Update to replite
    
    * Set kernel and code query string parameters
    
    * Update to prod URL
    
    * Enable the toolbar
    
    * Add back examples
    
    * Remove >> so the example code is copy pastable
    
    * Handle responsive
    
    * Update examples and mention Shift-Enter shortcut
    
    * Add back the enable button

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,3 @@
-function waitForKernel() {
-  let kernelInterval = setInterval(() => {
-    try {
-      if (thebeKernel) {
-        // Hide the enable button & copy, show the shell
-        $('#demo-code.fake-shell').css('display', 'none');
-        $('#numpy-shell.real-shell').css('display', 'flex');
-        $('#numpy-shell').addClass('numpy-shell-border');
-
-        // We need a more specific attribute to add the caret.
-        $('.thebelab-cell').attr('id', 'demo-code');
-        // Adds the caret
-        $('#numpy-shell #demo-code').prepend('<div class="demo-caret" aria-label="interactive shell prompt">&gt;</div>');
-
-        // Style the 'Run' button
-        $('.thebelab-button').each(function(idx) {
-          if (idx == 0) {
-            $(this).attr('id', 'demo-button-run');
-            $(this).attr('class', 'shell-button');
-            $(this).html('Run <span class="shift-enter">(Shift + Enter)</span>');
-          } else {
-            $(this).remove();
-          }
-        });
-        // Style the output elements
-        $('.jp-OutputArea').parent().closest('div').attr('id', 'demo-output-parent');
-        $('.jp-OutputArea').attr('id', 'demo-output');
-
-        // Show the lesson & hide the 'wait' text
-        $('.shell-wait').css('display', 'none');
-        $('.shell-lesson').css('display', 'flex');
-        clearInterval(kernelInterval);
-      }
-    } catch (err) {
-      if (err != 'ReferenceError: thebeKernel is not defined') {
-        console.log('Error loading the shell: ', err)
-      }
-    }
-  }, 500);
-}
-
-function loadShell() {
-  $('#shell-loader').css('display', 'inline-block');
-  // Add 'wait' text
-  $('.shell-intro').css('display', 'none');
-  $('.shell-wait').css('display', 'flex');
-  thebelab.bootstrap();
-  waitForKernel();
-}
-
 // Email Form
 $('.sign-up-input').focus(function(e) {
   if ($(window).width() > 850) {
@@ -59,6 +9,10 @@ $('.sign-up-input').focus(function(e) {
   }
 });
 
+function loadShell() {
+  $('#fake-shell').css('display', 'none');
+  $('#numpy-shell').css('display', 'flex');
+}
 
 function sendThankYou() {
   // Hides the email form to show a thank you
